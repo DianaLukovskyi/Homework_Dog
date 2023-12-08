e87c82900de4d602c1a42c4ea259ebc214369698
DianaLukovskyi <151841443+DianaLukovskyi@users.noreply.github.com>
2023-12-08 07:51:48 +0300

    Homework Dog

---
+++ src/homework/Dog.java
@@ -0,0 +1,26 @@
+package homework;
+
+public class Dog {
+   private String name;
+   private static int counter = 0;
+
+   public Dog (String name){
+       this.name=name;
+       counter++;
+
+
+
+
+    }
+    public static int getCounter(){
+       return counter;
+    }
+    public String getName(){
+       return name;
+
+    }
+
+
+
+}
+
---
+++ src/homework/Main1.java
@@ -0,0 +1,12 @@
+package homework;
+
+public class Main1 {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog ("Malchik");
+        Dog dog2 = new Dog ("Maya");
+        Dog dog3 = new Dog ("Roy");
+        System.out.println("Number of objects Dog " + Dog.getCounter());
+
+    }
+
+}
